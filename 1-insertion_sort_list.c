6ee09dca912756c59d6c1dab613fb3b490267994
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-10-26 13:20:57 +0000

    insertion algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,29 +7,29 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *node, *current, *tmp;
+	listint_t *node, *current, *tmp;
 
-    if (list == NULL || (*list)->next == NULL)
-        return;
-    node = (*list)->next;
-    while (node != NULL)
-    {
-        current = node;
-        while (current->prev && current->prev->n > current->n)
-        {
-            tmp = current->prev;
-            current->prev = tmp->prev;
-            tmp->next = current->next;
-            current->next = tmp;
-            tmp->prev = current;
-            if (tmp->next)
-                tmp->next->prev = tmp;
-            if (current->prev)
-                current->prev->next = current;
-            else
-                *list = current;
-            print_list(*list);
-        }
-        node = node->next;
-    }
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node != NULL)
+	{
+		current = node;
+		while (current->prev && current->prev->n > current->n)
+		{
+			tmp = current->prev;
+			current->prev = tmp->prev;
+			tmp->next = current->next;
+			current->next = tmp;
+			tmp->prev = current;
+			if (tmp->next)
+				tmp->next->prev = tmp;
+			if (current->prev)
+				current->prev->next = current;
+			else
+				*list = current;
+			print_list(*list);
+		}
+		node = node->next;
+	}
 }
